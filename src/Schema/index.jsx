74193c4845a3c82df55be59ce5da1c909409b5d8
Mitul  Kumar Chauhan <chauhanmitul466@gmail.com>
2025-07-01 20:34:17 +0530

    Update index.jsx

--- src/Schema/index.jsx
+++ src/Schema/index.jsx
@@ -1,9 +1,16 @@
 import * as yup from 'yup';
 
-export const signUpschema = yup.object({
+export const Schema = yup.object({
     name: yup.string().min(2).max(25).required("please enter your name"),
      email: yup.string().email().required("please enter your email"),
       password: yup.string().min(6).required("please enter your password"),
        confirm_password: yup.string().required().
        oneOf([yup.ref('password'),null] ,"password must match"),
+})
+
+export const LoginSchema = yup.object({
+    email: yup.string().email("invalid email").required("email is reruired"),
+    password:yup.string().required("password is required"),
+    confirm_password:yup.string()
+    .oneOf([yup.ref("password"),null], "password must match").required("confirm password is required ") 
 })
