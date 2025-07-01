e5cd3c28285e634542d3fbb8a30fc74c553b097f
Mitul  Kumar Chauhan <chauhanmitul466@gmail.com>
2025-07-01 20:36:51 +0530

    Update sign-up.jsx

--- src/Components/sign-up.jsx
+++ src/Components/sign-up.jsx
@@ -1,11 +1,14 @@
 
 
 import { ErrorMessage, useFormik } from "formik";
+import {Link} from "react-router-dom";
 
-import { signUpschema } from "../Schema";
+import { Schema } from "../Schema";
+import { useState } from "react";
 
-function SignupForm(){
+function Sign_Up(){
 
+  const [submitted , setSubmitted] = useState(false);
 
     const {values , errors, touched, handleSubmit , handleBlur , handleChange} = useFormik({
         initialValues:{
@@ -15,12 +18,13 @@ function SignupForm(){
             confirm_password:'',
         },
 
-        validationSchema:signUpschema,
+        validationSchema:Schema,
 
         onSubmit: (values , action) => {
-           
+           setSubmitted(true);
             console.log(values);
            action.resetForm();
+           setTimeout (() => setSubmitted(false), 3000)
 
         },});
        
@@ -30,25 +34,32 @@ function SignupForm(){
 
         return (
             <>
-            <div className="min-h-screen h-full flex item-center justify-center bg-gray-100 px-4">
+            <div className="min-h-screen h-full flex items-center justify-center bg-gray-100 px-4">
                 <div className="bg-white shadow-lg rounded-2xl p-8 w-full max-w-md mb-45 mt-20 shadow-cyan-700">
                     <h2 className="text-2xl font-bold text-center text-blue-600 mb-4">Create an Account</h2>
+                    {submitted && ( <div className= "text-green-500 text-center mb-4 font-semibold"> Account created successfully !</div>)}
                     <form onSubmit={handleSubmit}>
                         <div className="mb-4">
-                            <label htmlFor="name" className="block text-gray-600">Full Name</label>
-                        <input className="w-full mt-1 p-2 border rounded-lg" type="text" name="name" 
+                            <label htmlFor="name" className="block text-gray-600"> Full Name </label>
+                        <input className={`w-full mt-1 p-2 border rounded-lg ${errors.name && touched.name ? "border-red-500" : "border-gray-300"}`}
+                        id="name"
+                        autoComplete="name"
+                        type="text" 
+                        name="name" 
                         placeholder="enter your name"
                         value={values.name}
                         onChange={handleChange}
                              onBlur={handleBlur}
                            />
                            {errors.name && touched.name ? (
-                           <p className="form_error text-red-400 " >{errors.name}</p>): null}
+                           <p className="form_error  text-red-600 " >{errors.name}</p>): null}
                         </div>
                         <div className="mb-4">
                             <label htmlFor="email" className="block text-gray-600">E-mail</label>
                              <input 
-                             className="w-full mt-1 p-2 border rounded-lg " 
+                             className={`w-full mt-1 p-2 border rounded-lg ${errors.name && touched.name ? "border-red-500" : "border-gray-300"}`}
+                             id="emil"
+                             autoComplete="email"
                              type="email" 
                              name="email"
                                placeholder="enter your email"
@@ -57,13 +68,15 @@ function SignupForm(){
                              onBlur={handleBlur}
                             /> 
                             {errors.email && touched.email ? (
-                           <p className="form_error text-red-400" >{errors.email}</p>): null}
+                           <p className="form_error  text-red-600" >{errors.email}</p>): null}
                             
                         </div>
                           <div className="mb-4">
                             <label htmlFor="password" className="block text-gray-600">Password</label>
                              <input 
-                             className="w-full mt-1 p-2 border rounded-lg" 
+                             className={`w-full mt-1 p-2 border rounded-lg ${errors.name && touched.name ? "border-red-500" : "border-gray-300"}`} 
+                             id="password"
+                             autoComplete="current-password"
                              type="password" 
                              name="password"  
                                placeholder="enter your  password"
@@ -72,14 +85,16 @@ function SignupForm(){
                              onBlur={handleBlur}
                              /> 
                              {errors.password && touched.password ? (
-                           <p className="form_error text-red-400" >{errors.password}</p>): null}
+                           <p className="form_error  text-red-600" >{errors.password}</p>): null}
                              
                         </div>
                          
                           <div className="mb-4">
                             <label htmlFor="password" className="block text-gray-600">Confirm_Password</label>
                              <input 
-                             className="w-full mt-1 p-2 border rounded-lg" 
+                             id="comfirm_password"
+                             autoComplete="current-password"
+                             className={`w-full mt-1 p-2 border rounded-lg ${errors.name && touched.name ? "border-red-500" : "border-gray-300"}`}
                              type="password" 
                              name="confirm_password" 
                              placeholder="enter your confirm password"
@@ -88,15 +103,15 @@ function SignupForm(){
                              onBlur={handleBlur}
                       /> 
                       {errors.confirm_password && touched.confirm_password ? (
-                           <p className="form_error text-red-400" >{errors.confirm_password}</p>): null}
+                           <p className="form_error text-red-600" >{errors.confirm_password}</p>): null}
                             
                         </div>
    
 
                         <button
-                        type="submit" className= " w-25 bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">Sign Up</button>
+                        type="submit" className= " w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">Sign Up</button>
                     
-                    <span className="mt-4 flex" >Already have an account ?  </span>
+                    <span className="mt-4 flex" >Already have an account ?    <Link className="text-blue-800 underline" to="/Login">  Login here</Link>  </span>
                     </form>
                 </div>
 
@@ -109,4 +124,4 @@ function SignupForm(){
     
 
 
-export default SignupForm
+export default Sign_Up
