10d761a839fc825a3f54f067148d140060ede7f5
Mitul  Kumar Chauhan <chauhanmitul466@gmail.com>
2025-07-01 20:35:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import SignupForm from "./Components/sign-up"
+import {BrowserRouter, Routes , Route} from "react-router-dom";
 
+import Login from "./Components/Login";
+import Sign_up from "./Components/Sign_up"
 function App(){
   return(
-    <>
-  <div>
-    <SignupForm/>
-  </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Sign_up />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+    
+ </BrowserRouter>
        
   );
 }
 
 export default App
-
